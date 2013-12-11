5ab3aa7e25ccebf66561f87250f71a7f0885150a
Ben Martin <tennisracket22@yahoo.com>
2013-12-11 15:14:05 -0800

    Play "Free Fallin" when fifth page is reached

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 
 // Down arrow key to show more content
 
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
 window.onload = function(){
+
+	// document.getElementsByTagName('iframe')[0].play();
 
 	// Prevent keydown of spacebar scrolling the page
 	document.body.onkeydown = function(e) { 
@@ -21,16 +27,12 @@ window.onload = function(){
 		}
 		else if (e.keyCode === 32) {
 			
-			window.scrollTo(0, document.body.scrollTop + 723);
+			window.scrollTo(0, document.body.scrollTop + 722);
 
 			debugger;
 			var page = document.getElementsByClassName('page-number')[0].innerHTML;
 			var pageNumber = +page[0];
 			document.getElementsByClassName('page-number')[0].innerHTML = Math.min(pageNumber + 1, 5) + '/5';
-
-			e.preventDefault();
-			e.stopPropagation();
-			return false;
 		}
 	}
 }
@@ -43,17 +45,19 @@ window.onscroll = function(){
 	var page = Math.max(Math.ceil(document.body.scrollTop / 720), 1);
 	pageNumber.innerHTML = page + '/5';
 
-	// switch (true) {
-	// 	case (scrollPos <= 723):
-	// 		pageNumber.innerHTML = '1/5'
-	// 		break;
-	// 	case (scrollPos <= 1446):
-	// 		pageNumber.innerHTML = '2/5'
-	// 		break;
-	// 	default:
-	// 		break;
-	// }
 
-	console.log(document.body.scrollTop);
+	if (+pageNumber.innerHTML[0] === 5){
+		console.log();
+		document.getElementsByTagName('audio')[0].currentTime = 62;
+		document.getElementsByTagName('audio')[0].volume = 0;
+
+		var interval = setInterval(function(){
+			document.getElementsByTagName('audio')[0].volume = document.getElementsByTagName('audio')[0].volume + 0.05;
+		}, 50);
+
+		setTimeout(function(){clearInterval(interval)}, 1000);
+
+		document.getElementsByTagName('audio')[0].play();
+	}
 
 };
