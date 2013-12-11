610b4bc86e20408a5d4d8de127781427a5a1e7f1
Ben Martin <tennisracket22@yahoo.com>
2013-12-11 12:09:52 -0800

    Initial markup/styling

---
+++ script.js
@@ -0,0 +1,9 @@
+// Listen for space bar event and show more content by sliding the window down a certain amount
+
+// Down arrow key to show more content
+document.body.onkeyup = function(e){
+
+	if (e.keyCode === 13) console.log('hi');
+
+	
+}
